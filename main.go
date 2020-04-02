2bbcb58c9d6cedfe2aa394ae8d855159a62545f0
Odohi David <odohi.david@gmail.com>
2020-04-02 09:35:10 +0100

    create handler to handle sessions creation

--- main.go
+++ main.go
@@ -2,25 +2,44 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
 
+	"github.com/spankie/pointer/models"
 	"github.com/spankie/pointer/router"
 )
 
 func main() {
 	handler := router.NewRouter()
+	handler.SetContentType("application/json")
 	handler.POST("/session", func(w http.ResponseWriter, r *http.Request) {
+		var session models.Session
 		// get the session and save to the database
-		w.Write([]byte("Hello world"))
+		err := json.NewDecoder(r.Body).Decode(&session)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		session.ID = 13
+		fmt.Println(session)
+		js, err := json.Marshal(session)
+		if err != nil {
+			fmt.Printf("Error while marshaling json: %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("{error: 'internal server error'}"))
+			return
+		}
+		w.Write(js)
 	})
 	server := &http.Server{
 		Addr:    "localhost:8080",
 		Handler: handler,
 	}
 	gracefulShutdown(server)
+	fmt.Printf("server started at %s\n", server.Addr)
 	fmt.Println(server.ListenAndServe())
 }
 
