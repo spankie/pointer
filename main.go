1a9244c1629952a43d44ccf977250e58727e20c7
Odohi David <odohi.david@gmail.com>
2020-04-02 02:25:07 +0100

    gracefuly shutdown the app when ctrl c is entered on the command line

--- main.go
+++ main.go
@@ -4,20 +4,36 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"time"
+	"os"
+	"os/signal"
 
 	"github.com/spankie/pointer/router"
 )
 
 func main() {
 	handler := router.NewRouter()
-	handler.GET("/hello", func(w http.ResponseWriter, r *http.Request) {
+	handler.POST("/session", func(w http.ResponseWriter, r *http.Request) {
+		// get the session and save to the database
 		w.Write([]byte("Hello world"))
 	})
-	server := http.Server{
+	server := &http.Server{
 		Addr:    "localhost:8080",
 		Handler: handler,
 	}
-	time.AfterFunc(5*time.Second, func() { server.Shutdown(context.Background()) })
+	gracefulShutdown(server)
 	fmt.Println(server.ListenAndServe())
+}
+
+// gracefulShutdown shutsdown the server gracefully
+// when ctrl c is sent from the command line
+func gracefulShutdown(s *http.Server) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			// sig is a ^C, handle it
+			s.Shutdown(context.Background())
+			fmt.Println("server shutdown successfully")
+		}
+	}()
 }
