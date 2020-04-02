d377c1bb1237ce1e47a289ad9d8a2a1804f6a0e8
Odohi David <odohi.david@gmail.com>
2020-04-02 09:33:18 +0100

    add fields to the session model

--- models/session.go
+++ models/session.go
@@ -2,8 +2,8 @@ package models
 
 // Session holds details of a particular session
 type Session struct {
-	ID     uint64
-	Name   string
-	Users  []User
-	Status string
+	ID     uint64 `json:"id"`
+	Name   string `json:"name"`
+	Users  []User `json:"users"`
+	Status string `json:"status"`
 }
