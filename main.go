c2e1b3871b36c3807f078070bb3f5d76fccd7c4d
Odohi David <odohi.david@gmail.com>
2020-04-04 05:33:37 +0100

    refactor main to use the server package

--- main.go
+++ main.go
@@ -1,58 +1,18 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"os"
-	"os/signal"
-
-	"github.com/spankie/pointer/models"
+	"github.com/spankie/pointer/db"
 	"github.com/spankie/pointer/router"
+	"github.com/spankie/pointer/server"
 )
 
 func main() {
-	handler := router.NewRouter()
-	handler.SetContentType("application/json")
-	handler.POST("/session", func(w http.ResponseWriter, r *http.Request) {
-		var session models.Session
-		// get the session and save to the database
-		err := json.NewDecoder(r.Body).Decode(&session)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		session.ID = 13
-		fmt.Println(session)
-		js, err := json.Marshal(session)
-		if err != nil {
-			fmt.Printf("Error while marshaling json: %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("{error: 'internal server error'}"))
-			return
-		}
-		w.Write(js)
-	})
-	server := &http.Server{
-		Addr:    "localhost:8080",
-		Handler: handler,
+	s := &server.Server{
+		DB: db.DB{
+			Sessions: make(db.Sessions, 1),
+			Users:    make(db.Users, 1),
+		},
+		Router: router.NewRouter(),
 	}
-	gracefulShutdown(server)
-	fmt.Printf("server started at %s\n", server.Addr)
-	fmt.Println(server.ListenAndServe())
-}
-
-// gracefulShutdown shutsdown the server gracefully
-// when ctrl c is sent from the command line
-func gracefulShutdown(s *http.Server) {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		for range c {
-			// sig is a ^C, handle it
-			s.Shutdown(context.Background())
-			fmt.Println("server shutdown successfully")
-		}
-	}()
+	s.Start()
 }
