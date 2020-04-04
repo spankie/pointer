7607975d1c194b2af7a6ea49587472a0b8d6766c
Odohi David <odohi.david@gmail.com>
2020-04-04 05:32:48 +0100

    add a db package

--- db/db.go
+++ db/db.go
@@ -2,4 +2,14 @@ package db
 
 import "github.com/spankie/pointer/models"
 
-type Sessions map[int]models.Session
+// Sessions holds a list of all session identified by thier IDs
+type Sessions []models.Session
+
+// Users holds a list of all users identified by thier IDs
+type Users []models.User
+
+// DB provides access to the different db
+type DB struct {
+	Sessions Sessions
+	Users    Users
+}
