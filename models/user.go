4c3762280c121eb551b98effd6c1b2bd8d5fe9cd
Odohi David <odohi.david@gmail.com>
2020-04-04 05:33:03 +0100

    add fields to the user model

--- models/user.go
+++ models/user.go
@@ -2,7 +2,8 @@ package models
 
 // User holds a user details
 type User struct {
-	ID   uint64
-	Name string
-	Type string
+	ID        uint64 `json:"id"`
+	Name      string `json:"name"`
+	Type      string `json:"type"`
+	SessionID int    `json:"session_id"`
 }
